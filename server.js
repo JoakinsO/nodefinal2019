d5a97542f1b090ee5132d6bf7c84e536aa77e6fe
joakinsonyango <joakinsonyango05@gmail.com>
2019-04-16 18:30:55 -0700

    Update server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const hbs = require('hbs');
+const port = process.env.port || 8080;
+
+var app = express();
+
+app.set("view engine", "hbs");
+
+hbs.registerPartials(__dirname + '/views/partials/');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+app.get("/", (request, response) => {
+	response.render('index.hbs', {
+		title: "About page",
+		welcome: "Hello!"
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Server is up on the port ${port}`);
+})
