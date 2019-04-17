b9a243b8daa118edfe3bab5fb19216328f7cb60e
joakinsonyango <joakinsonyango05@gmail.com>
2019-04-17 00:36:02 -0700

    updates made

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ var app = express();
 app.set("view engine", "hbs");
 
 hbs.registerPartials(__dirname + '/views/partials/');
+app.use(express.static(__dirname, + '/public/'));
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
 app.get("/", (request, response) => {
-	response.render('index.hbs', {
-		title: "About page",
+	response.render('input.hbs', {
 		welcome: 'Hello Heroku!'
 	});
 });
