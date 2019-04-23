b5997b7eb8f958fa9e0705091ec50040481943ff
joakinsonyango <joakinsonyango05@gmail.com>
2019-04-23 14:46:00 -0700

    try

---
+++ server 2.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const imgpage = require('./imageapi');
+const cardpage = require('./cards');
+const port = process.env.PORT || 8080;
+
+var app = express();
+
+app.set("view engine", "hbs");
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+app.use(bodyParser.json());
+
+
+hbs.registerPartials(__dirname + '/views/partials/');
+app.use(express.static(__dirname, + '/public/'));
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+app.get("/", (request, response) => {
+	response.render('input.hbs', {
+		title: "MyPage",
+		welcome: 'Hello Heroku!'
+	});
+});
+
+
+app.post('/card_form', urlencodedParser, async (request, response) => {
+    try {
+        let cards = await cardpage.getCards(request.body.card);
+        response.render('page2.hbs', {
+            title: "DeckCards",
+            objects: cards
+        });
+    }catch (e) {
+        response.render('page2.hbs', {
+            output: e
+        });
+    }
+
+});
+
+
+app.post('/image_form', urlencodedParser, async (request, response) => {
+    try {
+        let imagereq = await imgpage.getImages(request.body.image);
+        response.render('page1.hbs', {
+            title: "NasaGallery",
+            objects: imagereq
+
+        });
+    }catch (e) {
+        response.render('page1.hbs', {
+            output: e
+        });
+    }
+
+});
+
+
+
+app.listen(port, () => {
+	console.log(`Server is up on the port ${port}`);
+})
